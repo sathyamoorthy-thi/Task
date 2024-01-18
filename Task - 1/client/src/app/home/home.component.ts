7ffc24a50a3dfca0339e19669971af5e23c49c44
Sathyamoorthy Thi <sathyamoorthy.thi@aspiresys.com>
2024-01-18 14:40:47 +0530

    one

---
+++ Task - 1/client/src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode=false;
+  users:any;
+  constructor(private http:HttpClient){
+
+  }
+  ngOnInit(): void {
+    this.getUsers();
+  }
+  getUsers(){
+    this.http.get('http://localhost:5001/api/Users').subscribe({
+      next:response=>this.users=response,
+      error: error=> console.log(error),
+      complete: ()=>console.log("Request is completed")
+    })
+  }
+  registerToggle(){
+    this.registerMode=!this.registerMode;
+  }
+  cancelRegister(event:boolean){
+    this.registerMode=event;
+  }
+
+}
